9caa9d57c8a5e8d92d8426670398aea97a3d5102
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2024-10-30 21:12:48 -0300

    Criei o header

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Logo from "../img/logo.png"
+import Light_mode from "../img/light_mode.svg"
+import Dark_mode from "../img/dark_mode.svg"
+
+
+export default function Header() {
+    return (
+        <header>
+            <div className="box1">
+                <img src={Logo} alt="Logo da Página" id="logo" />
+                <nav>
+                    <a href="#">Página Inicial</a>
+                    <a href="#">Sobre</a>
+                    <a href="#">Projetos</a>
+                    <a href="#">Habilidades</a>
+                    <a href="#">Contato</a>
+
+                    <button id="dark_mode"><img src={Dark_mode} alt="Botão do modo claro"/></button>
+                </nav>
+            </div>
+        </header>
+
+    );
+};
